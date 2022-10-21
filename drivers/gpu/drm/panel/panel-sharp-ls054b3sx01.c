f1c01bbdb7d4539eac2221f2650a14201860d928
Brooksytech <macebrooks@gmail.com>
2022-10-21 08:03:21 -0700

    Fix panel flicking on the RG552

--- drivers/gpu/drm/panel/panel-sharp-ls054b3sx01.c
+++ drivers/gpu/drm/panel/panel-sharp-ls054b3sx01.c
@@ -234,17 +234,18 @@ static int sharp_ls054_unprepare(struct drm_panel *panel)
 
 // FIXME: should we strive for perfect 60hz?
 static const struct drm_display_mode sharp_ls054_mode = {
-	.clock		= ((1152 + 64 + 8 + 28) * (1920 + 56 + 3 + 6) * 60) / 1000,
+	.clock		= 150000,
 	.hdisplay	= 1152,
 	.hsync_start	= 1152 + 64,
-	.hsync_end	= 1152 + 64 + 8,
-	.htotal		= 1152 + 64 + 8 + 28,
+	.hsync_end	= 1152 + 64 + 4,
+	.htotal		= 1152 + 64 + 4 + 32,
 	.vdisplay	= 1920,
-	.vsync_start	= 1920 + 56,
-	.vsync_end	= 1920 + 56 + 3,
-	.vtotal		= 1920 + 56 + 3 + 6,
-	.width_mm	= 70,
-	.height_mm	= 117,
+	.vsync_start	= 1920 + 3,
+	.vsync_end	= 1920 + 3 + 3,
+	.vtotal		= 1920 + 3 + 3 + 4,
+	.width_mm	= 69,
+	.height_mm	= 116,
+	.flags		= DRM_MODE_FLAG_NHSYNC | DRM_MODE_FLAG_NVSYNC,
 };
 
 static int sharp_ls054_get_modes(struct drm_panel *panel,
